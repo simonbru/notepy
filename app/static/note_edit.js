0c51c77d9a4275a9acf799dae2ddfc93fb8fa9fe
Simon Brulhart <simon@brulhart.me>
2014-11-28 21:59:05 +0100

    JS: watch noteContent for changes

--- app/static/note_edit.js
+++ app/static/note_edit.js
@@ -1,13 +1,32 @@
 function Note(notename) {
-	// Bindings
+	// Bind to the DOM elements
 	this._nodeNote = $('#note_content');
 	
+	// Properties
 	this._jqxhr = null;
 	this._timeout = null;
 	this.last_save = new Date();
 	this.last_content = this._nodeNote.val();
+	
+	// Create events
+	var $this = this;
+	this._nodeNote.bind("input cut paste", function(e) {
+		if (!$this.has_changed()) return;
+		console.log("change: ", $this.has_changed(), e);
+	});
 }
 
+Note.prototype.current_content = function() {
+	return this._nodeNote.val();
+}
+
+Note.prototype.has_changed = function() {
+	//debugger;
+	return this.last_content != this.current_content();
+}
+
+
+// Old test function
 function save() {
 	$.post('/api/notes/{{note_name}}/put', {
 			note_content: $('textarea').val()
--- app/templates/note_edit.tpl
+++ app/templates/note_edit.tpl
@@ -5,4 +5,9 @@
 <br><br>
 <textarea class="form-control" id="note_content">{{note_content}}</textarea>
 <script type="text/javascript" src="/static/note_edit.js"></script>
+<script type="text/javascript">
+document.addEventListener("DOMContentLoaded", function(event) {
+	window.note = new Note("{{note_name}}");
+});
+</script>
 % include("footer")
